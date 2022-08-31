6087d741bf42955129f1513301086c59e5ec7da8
Erick Lincoln <ericklincoln@live.com>
2022-08-31 08:33:57 -0300

    Update

--- math_pitagoras.py
+++ math_pitagoras.py
@@ -3,6 +3,6 @@ import math
 def calc_pitagoras(LadoA, LadoB, LadoC):
     LadoA = int(input('Insira o valor:'))
     LadoB = int(input('Insira o valor:'))
-    adoC = math.sqrt(LadoA*LadoA + LadoB*LadoB)
+    LadoC = math.sqrt(LadoA*LadoA + LadoB*LadoB)
 
     print(LadoA, LadoB, LadoC)
